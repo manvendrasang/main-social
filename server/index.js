e91ba4760bcadc7d12e0668818bfe95ddba95c5f
Manvendra <manvendrasang@gmail.com>
2025-07-19 14:15:14 +0530

    Initialized file storage and made connection with MongoDB via Mongoose

--- server/index.js
+++ server/index.js
@@ -28,3 +28,24 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors()); 
 app.use("/assets", express.static(path.join(__dirname, 'public/assets'))); 
 
+// FILE STORAGE
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({ storage }); 
+
+// MONGOOSE SETUP
+
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`SERVER PORT: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`));
