951f31b7b5e5302360396d5d1933e3b8b3f42a57
Manvendra <manvendrasang@gmail.com>
2025-07-19 14:14:26 +0530

    Added Middleware in index.js

--- server/index.js
+++ server/index.js
@@ -17,3 +17,14 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 
+// MIDDLEWARE
+
+app.use(express.json()); 
+app.use(helmet()); 
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); 
+app.use(morgan("common")); 
+app.use(bodyParser.json({ limit: "30mb", extended: true })); 
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); 
+app.use(cors()); 
+app.use("/assets", express.static(path.join(__dirname, 'public/assets'))); 
+
