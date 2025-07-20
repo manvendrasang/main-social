05cb981a55c0e70de8ab22c30a542b2b59334446
Manvendra <manvendrasang@gmail.com>
2025-07-21 01:27:11 +0530

    User routes setup done

--- server/index.js
+++ server/index.js
@@ -10,18 +10,17 @@ import path from "path";
 
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
-import authRoutes from "./router/auth.js"
+import authRoutes from "./route/auth.js"
 import { verifyToken } from "./middleware/auth.js";
+import userRoutes from "./routes/users.js";
 
 // CONFIGURATIONS
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 
 // MIDDLEWARE
-
 app.use(express.json()); 
 app.use(helmet()); 
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); 
@@ -32,7 +31,6 @@ app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets'))); 
 
 // FILE STORAGE
-
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, "public/assets");
@@ -44,15 +42,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage }); 
 
 // ROUTES WITH FILES
-
 app.post("/auth/register", upload.single("picture"), register)
 
 // ROUTES
-
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 // MONGOOSE SETUP
-
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
