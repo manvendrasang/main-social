24b3f018a5bde39d651fcf8c4f95e7eea0479a0e
Manvendra <manvendrasang@gmail.com>
2025-07-21 02:03:27 +0530

    Post routes boiler plate setup done

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ import { register } from "./controllers/auth.js";
 import authRoutes from "./route/auth.js"
 import { verifyToken } from "./middleware/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js"
+import { createPost } from "./controllers/posts.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -43,10 +45,12 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register)
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // ROUTES
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes)
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
